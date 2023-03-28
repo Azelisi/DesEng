438515f4428b7b4af4abaf6351668d479d1c40b5
Huinko <egor.markin.2019@mail.ru>
2023-03-29 02:18:43 +0500

    first

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Button, TouchableOpacity, ImageBackground, Text, View } from 'react-native';
+
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ImageBackground source={require('./assets/startImg.png')} resizeMode="cover" style={styles.image}>
+        <Text style={styles.startText}>Станьте частью {"\n"}
+          DesEng! </Text>
+        <Button style={styles.startButton}
+          title="Продолжить"
+          color="#FF6014"
+        />
+      </ImageBackground>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +23,23 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  startText: {
+    color: 'white',
+    fontSize: 28,
+  },
+  startButton: {
+    borderTopLeftRadius: 5,
+    borderTopRightRadius: 5,
+    borderBottomLeftRadius: 5,
+    borderBottomRightRadius: 5,
   },
 });
