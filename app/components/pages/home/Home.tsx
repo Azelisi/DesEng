ef7c60b8cbf2b01824081e3d93df9b46a340f77a
Huinko <egor.markin.2019@mail.ru>
2023-05-15 02:52:10 +0500

    Виджет слово дня

--- app/components/pages/home/Home.tsx
+++ app/components/pages/home/Home.tsx
@@ -1,25 +1,135 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Image, Text, StyleSheet,TouchableOpacity } from 'react-native';
+import { SafeAreaProvider, useSafeAreaInsets } from 'react-native-safe-area-context';
 
-const Home = () => {
+
+const Home = ({ navigation, username, avatar, word   }) => {
+    
     return (
+        <SafeAreaProvider>
         <View style={styles.container}>
-            <Text style={styles.text}>Страница находится в разработке </Text>
-        </View>
+         <Header username="ivanagopov74 " avatar="https://bestappsforkids.com/wp-content/uploads/2017/10/Frog.jpg" />
+         
+        <WordOfTheDay word = "Usability [ˌjuːzəˈbɪləti]  " translation="Практичность [Юзабилити] " navigation={navigation}/>
+        <Course course = "Пройти «Logotype» урок из курса «Фирменный стиль»" time="Сегодня 15:00 Грамматика " navigation={navigation}/>
+        
+      </View>
+      </SafeAreaProvider>
+    
     );
 };
 
+const Header = (props) => {
+    const { username, avatar } = props;
+    const insets = useSafeAreaInsets();
+  
+    return (
+        <View style={{
+            marginLeft:20,
+            flexDirection: 'row',
+            alignItems: 'center',
+            paddingTop: insets.top}}>
+        <Image source={{ uri: avatar }} style={styles.img} />
+        <Text style={styles.container}>{username}</Text>
+      </View>
+    );
+  };
+
+  const WordOfTheDay = ({ word, translation, navigation }) => {
+    return (
+      <View style={styles.container}>
+          <View style={styles.title}>
+            <Text style={styles.titleText}>Слово дня</Text>
+            <View style={styles.divider} />
+            <TouchableOpacity onPress={() => navigation.navigate('Словарь')}>
+              <Text style={styles.titleText}>Словарь</Text>
+            </TouchableOpacity>
+          </View>
+        <View style={styles.borderVid}>
+          <View style={styles.word}>
+            <Text style={styles.wordText}>{word}</Text>
+            <Text style={styles.wordTranslation}>{translation}</Text>
+          </View>
+        </View>
+      </View>
+    );
+  };
+
+  
+  const Course = ({ course, time, navigation }) => {
+    return (
+      <View style={styles.container}>
+          <View style={styles.title}>
+            <Text style={styles.titleText}>Назначенный курс</Text>
+            <View style={styles.divider} />
+            <TouchableOpacity onPress={() => navigation.navigate('Задания')}>
+              <Text style={styles.titleText}>Дневник</Text>
+            </TouchableOpacity>
+          </View>
+        <View style={styles.borderVid}>
+          <View style={styles.word}>
+            <Text style={styles.wordTranslation}>{time}</Text>
+            <Text style={styles.wordText}>{course}</Text>
+          </View>
+        </View>
+      </View>
+    );
+  };
+
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#616161',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    text: {
-        fontSize: 20,
-        color: '#fff',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#616161',
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: '#fff',
+  },
+  text: {
+    fontSize: 20,
+  },
+  img: {
+    width: 30,
+    height: 30,
+    borderRadius: 20,
+    marginRight: 10,
+  },
+  title: {
+    backgroundColor: '#545454',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderWidth: 1,
+    paddingRight:30,
+  },
+  titleText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+    margin: 5,
+    marginLeft:30,
+  },
+  borderVid: {
+    paddingRight:100,
+  },
+  divider: {
+    flex: 1,
+  },
+  word: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    marginLeft:10,
+  },
+  wordText: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'bold',
+    margin:5,
+  },
+  wordTranslation: {
+    color: '#fff',
+    fontSize: 15,
+    margin:5,
+  },
 });
 
 export default Home; 
--- app/navigation/Navigation.js
+++ app/navigation/Navigation.js
@@ -18,6 +18,7 @@ import Dictionary from '../components/pages/home/Dictionary';
 import Tasks from '../components/pages/home/Tasks';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { AuthContext } from './AuthContext';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 
 const Stack = createNativeStackNavigator();
@@ -27,80 +28,83 @@ export default function Navigation() {
     const [isAuthenticated, setIsAuthenticated] = useState(false);
 
     return (
-        <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated }}>
-            <NavigationContainer>
-                {isAuthenticated ? (
-                    <Tab.Navigator screenOptions={{
-                        tabBarShowLabel: false,
-                        headerMode: 'screen',
-                        headerTintColor: 'white',
-                        headerStyle: { backgroundColor: '#616161' },
-                        inactiveTintColor: 'white',
-                        tabBarStyle: {
-                            backgroundColor: 'gray',
-                        },
-                        tabBarActiveTintColor: 'white',
-                    }}>
-                        <Tab.Screen name="Главная страница" component={Home}
-                            options={{
-                                tabBarIcon: ({ focused }) => (
-                                    <Image
-                                        source={focused ? require('../assets/home_active.png') : require('../assets/home.png')}
-                                        style={{ width: 28, height: 28 }}
-                                    />
-                                ),
-                            }} />
-                        <Tab.Screen name="Задания" component={Tasks}
-                            options={{
-                                tabBarIcon: ({ focused }) => (
-                                    <Image
-                                        source={focused ? require('../assets/pen_active.png') : require('../assets/pen.png')}
-                                        style={{ width: 28, height: 28 }}
-                                    />
-                                ),
-                            }} />
-                        <Tab.Screen name="Словарь" component={Dictionary}
-                            options={{
-                                tabBarIcon: ({ focused }) => (
-                                    <Image
-                                        source={focused ? require('../assets/book_active.png') : require('../assets/book.png')}
-                                        style={{ width: 28, height: 28 }}
-                                    />
-                                ),
-                            }} />
-                        <Tab.Screen name="Календарь" component={Calendar}
-                            options={{
-                                tabBarIcon: ({ focused }) => (
-                                    <Image
-                                        source={focused ? require('../assets/more_active.png') : require('../assets/more.png')}
-                                        style={{ width: 28, height: 28 }}
-                                    />
-                                ),
-                            }} />
-                    </Tab.Navigator>
-                ) : (
-                    <Stack.Navigator
-                        screenOptions={{
+        <SafeAreaProvider>
+            <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated }}>
+                <NavigationContainer>
+                    {isAuthenticated ? (
+                        <Tab.Navigator screenOptions={{
+                            tabBarShowLabel: false,
                             headerMode: 'screen',
                             headerTintColor: 'white',
                             headerStyle: { backgroundColor: '#616161' },
-                        }}
-                    >
-                        <Stack.Screen name="Начальный экран" component={StartPages} />
-                        <Stack.Screen name="Регистрация в DesEng" component={Registr} />
-                        <Stack.Screen name="Портал МИДиС" component={Midis} />
-                        <Stack.Screen name="Вход в приложение" component={Registr} />
-                        <Stack.Screen name="Направление" component={Direction} />
-                        <Stack.Screen name="Категории" component={Categories} />
-                        <Stack.Screen name="Уровень подготовки" component={Degree} />
-                        <Stack.Screen name="Тестирование" component={Test} />
-                        <Stack.Screen name="Завершение регистрации" component={Cool} />
-                        <Stack.Screen name="Профиль" component={Profile} />
-                        <Stack.Screen name="В разработке" component={NotFound} />
-                    </Stack.Navigator>
-                )}
-            </NavigationContainer>
-        </AuthContext.Provider>
+                            inactiveTintColor: 'white',
+                            tabBarStyle: {
+                                backgroundColor: 'gray',
+                            },
+                            tabBarActiveTintColor: 'white',
+                        }}>
+                            <Tab.Screen name="Главная страница" component={Home}
+                                options={{
+                                    headerShown: false,
+                                    tabBarIcon: ({ focused }) => (
+                                        <Image
+                                            source={focused ? require('../assets/home_active.png') : require('../assets/home.png')}
+                                            style={{ width: 28, height: 28 }}
+                                        />
+                                    ),
+                                }} />
+                            <Tab.Screen name="Задания" component={Tasks}
+                                options={{
+                                    tabBarIcon: ({ focused }) => (
+                                        <Image
+                                            source={focused ? require('../assets/pen_active.png') : require('../assets/pen.png')}
+                                            style={{ width: 28, height: 28 }}
+                                        />
+                                    ),
+                                }} />
+                            <Tab.Screen name="Словарь" component={Dictionary}
+                                options={{
+                                    tabBarIcon: ({ focused }) => (
+                                        <Image
+                                            source={focused ? require('../assets/book_active.png') : require('../assets/book.png')}
+                                            style={{ width: 28, height: 28 }}
+                                        />
+                                    ),
+                                }} />
+                            <Tab.Screen name="Календарь" component={Calendar}
+                                options={{
+                                    tabBarIcon: ({ focused }) => (
+                                        <Image
+                                            source={focused ? require('../assets/more_active.png') : require('../assets/more.png')}
+                                            style={{ width: 28, height: 28 }}
+                                        />
+                                    ),
+                                }} />
+                        </Tab.Navigator>
+                    ) : (
+                        <Stack.Navigator
+                            screenOptions={{
+                                headerMode: 'screen',
+                                headerTintColor: 'white',
+                                headerStyle: { backgroundColor: '#616161' },
+                            }}
+                        >
+                            <Stack.Screen name="Начальный экран" component={StartPages} />
+                            <Stack.Screen name="Регистрация в DesEng" component={Registr} />
+                            <Stack.Screen name="Портал МИДиС" component={Midis} />
+                            <Stack.Screen name="Вход в приложение" component={Registr} />
+                            <Stack.Screen name="Направление" component={Direction} />
+                            <Stack.Screen name="Категории" component={Categories} />
+                            <Stack.Screen name="Уровень подготовки" component={Degree} />
+                            <Stack.Screen name="Тестирование" component={Test} />
+                            <Stack.Screen name="Завершение регистрации" component={Cool} />
+                            <Stack.Screen name="Профиль" component={Profile} />
+                            <Stack.Screen name="В разработке" component={NotFound} />
+                        </Stack.Navigator>
+                    )}
+                </NavigationContainer>
+            </AuthContext.Provider>
+        </SafeAreaProvider>
     );
 }
 
